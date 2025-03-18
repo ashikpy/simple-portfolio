b44082596f98d5c4aff2068e08dc8652a54f50c4
Ashik D <87185937+ashikpy@users.noreply.github.com>
2025-03-18 10:56:32 +0530

    Update page.tsx

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -9,35 +9,35 @@ export default function Page() {
       <div className="flex flex-col  gap-4">
         <WorkContainer
           alt="hello"
-          imag="https://ashikpy.tech/ashsym.png"
+          imag="https://ashbio.vercel.app//ashsym.png"
           title="AshSym"
           desc="AshSym 2024 is a technical festival organized by the Science and Humanities department, featuring both technical and non-technical events. The festival aims to promote the exchange of ideas and knowledge between students, researchers, and industry professionals. I personally got the previleage to make this website, and I'm proud of the work I did. I hope you enjoy it as much as I do."
           redirectLink="https://ashsym.vercel.app"
         />
         <WorkContainer
           alt="hello"
-          imag="https://ashikpy.tech/aiyara.png"
+          imag="https://ashbio.vercel.app/aiyara.png"
           title="Aiyara"
           desc="Aiyara's website presents a vibrant showcase of their floral offerings, including wedding garlands, jasmine garlands, and custom bouquets. It highlights their expertise in floral arrangements for weddings and events, featuring high-quality images and customer testimonials. The site is user-friendly and designed to engage potential clients effectively."
           redirectLink="https://aiyara.vercel.app"
         />
         <WorkContainer
           alt="hello"
-          imag="https://ashikpy.tech/cybertoys.png"
+          imag="https://ashbio.vercel.app/cybertoys.png"
           title="CyberToys"
           desc="This is a website I made as a fun, with my cousin who's a mordern artist, this website is a cyberpunk themed website. It has been evolving since I started learning web development. I'm proud of the work I did. I'm stkill learning new things, and I'm making this website as a side project. I hope you enjoy it as much as I do. *This website is not finished yet.*"
           redirectLink="https://cybertoys.vercel.app"
         />
         <WorkContainer
           alt="hello"
-          imag="https://ashikpy.tech/cybertoys.png"
+          imag="https://ashbio.vercel.app/cybertoys.png"
           title="CyberToys"
           desc="This is a website I made as a fun, with my cousin who's a mordern artist, this website is a cyberpunk themed website. It has been evolving since I started learning web development. I'm proud of the work I did. I'm stkill learning new things, and I'm making this website as a side project. I hope you enjoy it as much as I do. *This website is not finished yet.*"
           redirectLink="https://cybertoys.vercel.app"
         />
         <WorkContainer
           alt="hello"
-          imag="https://ashikpy.tech/ashbio.png"
+          imag="https://ashbio.vercel.app//ashbio.png"
           title="Ashik's Portfolio"
           desc="My Portfolio website, this website is actually my first ever project. It has been evolving since I started learning web development. I'm proud of the work I did. I'm stkill learning new things, and I'm making this website as a side project. I hope you enjoy it as much as I do."
           redirectLink="https://ashbio.vercel.app"
