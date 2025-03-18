430395d8c70e04ca952188354d5209ef13e27cc1
Ashik D <87185937+ashikpy@users.noreply.github.com>
2025-03-18 11:35:31 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "ashikpy.tech"],
+    domains: ["avatars.githubusercontent.com", "ashikpy.tech", "ashbio.vercel.app"],
   },
   devIndicators: false,
 };
